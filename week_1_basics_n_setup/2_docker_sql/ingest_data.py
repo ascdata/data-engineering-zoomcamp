f0fa37e3fbbbeac924b2e1d8238559d17607bbc8
ascdata <107685588+ascdata@users.noreply.github.com>
2024-01-24 15:27:47 +0000

    - Customizing folder structure - Deleting not relevant files

--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -11,6 +11,7 @@ from sqlalchemy import create_engine
 
 
 def main(params):
+    # use the argpare module to parse command-line arguments
     user = params.user
     password = params.password
     host = params.host 
@@ -26,22 +27,30 @@ def main(params):
     else:
         csv_name = 'output.csv'
 
+    # execute a wget command to download the CSV file from URL
     os.system(f"wget {url} -O {csv_name}")
 
+    # create a PostgreSQL database connection using the create_engine function from sqlalchemy library
+    # connection parameters are taken from the command-line arguments
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
+    # use pandas to read the CSV file in chunks
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
+    # use first chunk to get the data types of the columns
     df = next(df_iter)
 
+    # convert two columns to datetime format
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
+    # creates an empty table in the PostgreSQL database with the specified name (table_name) and replaces it if it already exists.
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
+    # append the first chunk to the PostgreSQL table
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
-
+    # continue loading subsequent chunks into the PostgreSQL table
     while True: 
 
         try:
@@ -59,12 +68,15 @@ def main(params):
             print('inserted another chunk, took %.3f second' % (t_end - t_start))
 
         except StopIteration:
+            # print a message when the data ingestion process is complete
             print("Finished ingesting data into the postgres database")
             break
 
 if __name__ == '__main__':
+    # set up command-line argument parser
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
 
+    # define required command-line arguments
     parser.add_argument('--user', required=True, help='user name for postgres')
     parser.add_argument('--password', required=True, help='password for postgres')
     parser.add_argument('--host', required=True, help='host for postgres')
@@ -73,6 +85,8 @@ if __name__ == '__main__':
     parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
     parser.add_argument('--url', required=True, help='url of the csv file')
 
+    # Psparse command-line arguments
     args = parser.parse_args()
 
+    # call the main function with the parsed arguments
     main(args)
