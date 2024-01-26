3fb815803857c94490e93abdce2f9300b566c2a5
ascdata <107685588+ascdata@users.noreply.github.com>
2024-01-26 15:43:36 +0000

    - Customizing

--- week_1_basics_n_setup/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
+++ week_1_basics_n_setup/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
@@ -26,13 +26,13 @@ variable "location" {
 variable "bq_dataset_name" {
   description = "My BigQuery Dataset Name"
   #Update the below to what you want your dataset to be called
-  default     = "terra-dataset"
+  default     = "terra_dataset"
 }
 
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
   #Update the below to a unique bucket name
-  default     = "terra-bucket"
+  default     = "taxi-rides-ny-410014-terra-bucket"
 }
 
 variable "gcs_storage_class" {
